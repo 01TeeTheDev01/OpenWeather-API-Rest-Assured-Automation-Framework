498ac507aa50f247c1c773f600e556fe5fa98eb3
Teethedev@outlook.com <Teethedev@outlook.com>
2024-06-24 21:51:08 +0200

    Added database reader

---
+++ src/test/java/com/openweather/api/data/OpenWeatherDbReader.java
@@ -0,0 +1,69 @@
+package com.openweather.api.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class OpenWeatherDbReader {
+
+    private final String connString, userName, password;
+    private Connection connection;
+
+    public OpenWeatherDbReader(String connString, String userName, String password) {
+        this.connString = connString;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public void setupConnection(){
+        try{
+            connection = DriverManager.getConnection(connString, userName, password);
+
+            if(connection.isClosed()) {
+                System.out.println("""
+                         The connection to the server is currently non-operational.\s
+                        \s
+                         Unable to proceed with acquiring the APi key.
+                        \s""");
+
+            }
+
+        }catch (Exception ex){
+            System.out.printf("""
+                    Oops.... we've encountered an error: %s
+                    """,
+                    ex.getMessage());
+        }
+    }
+
+    public String getApiKey(){
+        try{
+            if(!connection.isClosed()){
+                var results = connection
+                        .createStatement()
+                        .executeQuery("""
+                    select *
+                    from public."OpenWeather"
+                    where Id = 1;
+                    """);
+
+                if(results == null ||
+                        results.wasNull() ||
+                        results.getString(1).isEmpty() ||
+                        results.getString(1).isBlank()){
+                    System.out.println("Failed to retrieve key from data source!");
+                    return "N/A";
+                }
+
+                return results.getString(1);
+            }
+
+            return "N/A";
+        }catch (Exception ex){
+            System.out.printf("""
+                    Oops.... we've encountered an error: %s
+                    """,
+                    ex.getMessage());
+            return "N/A";
+        }
+    }
+}
